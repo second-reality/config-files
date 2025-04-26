d2be99d1ef7b291c1d696483849ae7e06ff7a300
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-04-25 21:10:07 -0700

    up

--- .screenlayout/tv.sh
+++ .screenlayout/tv.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off
-xrandr --output HDMI-1 --set "Broadcast RGB" "Full"
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off
 xset s off
 xset -dpms
